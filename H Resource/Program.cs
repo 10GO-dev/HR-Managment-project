f75a1f86080d4e9e7fa38b349745c9bebe7002fe
10GO-dev <Diegoperalta0929@gmail.com>
2023-04-20 03:41:55 -0400

    "Project Finished"

--- H Resource/Program.cs
+++ H Resource/Program.cs
@@ -19,15 +19,13 @@ namespace H_Resource
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            //LoginView login = new LoginView();
-            //login.ShowDialog();
-            //if(login.DialogResult == DialogResult.OK)
-            //{
-            //    IMainContainer view = MainContainer.GetInstance();
-            //    _ = new MainContainerPresenter(view);
-            //}
-            IMainContainer view = MainContainer.GetInstance();
-            _ = new MainContainerPresenter(view);
+            LoginView login = new LoginView();
+            login.ShowDialog();
+            if (login.DialogResult == DialogResult.OK)
+            {
+                IMainContainer view = MainContainer.GetInstance();
+                _ = new MainContainerPresenter(view);
+            }
         }
     }
 }   
